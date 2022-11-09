2d8a0a60e042fa9403f79388716aacf5baf7f572
Ironknee <marekdarsa@gmail.com>
2022-11-09 14:50:39 +0100

    done

---
+++ Generato_vet/Generato_vet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generato_vet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vety vety = new Vety();
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(vety.GenerujVetu());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Generato_vet/Generato_vet/Vety.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generato_vet
+{
+    internal class Vety
+    {
+       
+
+        string[] podmety = { "jednorožec", "programátor", "manažer", "hroch", "T - rex" };
+        string[] prisudky = { "spal", "ležel", "vařil", "uklízel", "derivoval" };
+        string[] privlastky = { "modrý", "velký", "hubený", "nejlepší", "automatizovaný" };
+        string[] prislovce = { "rychle", "s oblibou", "hodně", "málo", "se zpožděním" };
+        string[] pum = { "pod stolem", "v lese", "u babičky", "v práci", "na stole" };
+
+        Random random = new Random();
+
+        public string GenerujVetu()
+        {
+            
+            return $"{podmety[random.Next(0, 5)]} {prisudky[random.Next(0, 5)]} {privlastky[random.Next(0, 5)]} {prislovce[random.Next(0, 5)]} {pum[random.Next(0, 5)]}";
+        }
+
+
+    }
+}
