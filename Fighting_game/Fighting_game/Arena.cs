c5a9b17de6b2d498c01d80fcfaae7da59591b3ad
MarekDarsa <maerkdarsa@gmail.com>
2022-11-14 18:44:35 +0100

    zakladni mechanika souboje hotova

--- Fighting_game/Fighting_game/Arena.cs
+++ Fighting_game/Fighting_game/Arena.cs
@@ -11,6 +11,8 @@ namespace Fighting_game
         public Fighter fighter1 { get; private set; }
         public Fighter fighter2 { get; private set; }
         bool zapas = true;
+        int utok;
+        int obrana;
 
         public Arena(Fighter fighter1, Fighter fighter2)
         {
@@ -23,23 +25,35 @@ namespace Fighting_game
         {
             while (zapas)
             {   //musím si tady dát utok i obranu do nových proměných abych je byl schopný vypsat
-                fighter2.Zraneni(fighter1.Zautoc() - fighter2.BranSe());
-                Console.WriteLine($"{fighter1} zaútočil utokem o sile {fighter1.Zautoc()} bojovnik {fighter2} se branil silou {fighter2.BranSe()}");
+                utok = fighter1.Zautoc();
+                obrana = fighter2.BranSe();
+
+                fighter2.Zraneni(utok - obrana);
+                Console.WriteLine($"{fighter1} zaútočil utokem o sile {utok} bojovnik {fighter2} se branil silou {obrana}");
                 if (fighter2.Zivoty() <= 0)
                 {
                     Console.WriteLine($"Vyhrál bojovník {fighter1}");
                     zapas = false;
-                }else
-                Console.WriteLine($"{fighter2} zbyvyjici zivoty: {fighter2.Zivoty()}");
-
-                fighter1.Zraneni(fighter2.Zautoc() - fighter1.BranSe());
-                Console.WriteLine($"{fighter1} zaútočil utokem o sile {fighter1.Zautoc()} bojovnik {fighter2} se branil silou {fighter2.BranSe()}");
-                if (fighter1.Zivoty()<=0)
+                }
+                else
                 {
-                    Console.WriteLine($"Vyhrál bojovník {fighter2}");
-                    zapas = false;
-                }else
-                Console.WriteLine($"{fighter1} zbyvyjici zivoty: {fighter1.Zivoty()}");
+
+                    Console.WriteLine($"{fighter2} zbyvyjici zivoty: {fighter2.Zivoty()}");
+
+                    utok = fighter2.Zautoc();
+                    obrana = fighter1.BranSe();
+
+                    fighter1.Zraneni(utok - obrana);
+
+                    Console.WriteLine($"{fighter2} zaútočil utokem o sile {utok} bojovnik {fighter1} se branil silou {obrana}");
+                    if (fighter1.Zivoty() <= 0)
+                    {
+                        Console.WriteLine($"Vyhrál bojovník {fighter2}");
+                        zapas = false;
+                    }
+                    else
+                        Console.WriteLine($"{fighter1} zbyvyjici zivoty: {fighter1.Zivoty()}");
+                }
 
 
 
