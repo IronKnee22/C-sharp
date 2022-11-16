326f437960c1993b9467ce02ddf8528b1f3a499c
Ironknee <marekdarsa@gmail.com>
2022-11-16 12:59:19 +0100

    zacatek Turnaje

--- Fighting_game/Fighting_game/Program.cs
+++ Fighting_game/Fighting_game/Program.cs
@@ -10,13 +10,23 @@ namespace Fighting_game
     {
         static void Main(string[] args)
         {
+
+            List<Fighter> fighters = new List<Fighter>();
+
             Fighter Jarda = new Fighter("Jarda", 100, 20,10);
-            Fighter Pepa = new Fighter("Pepik", 120, 15, 15);
-            Arena arena = new Arena(Jarda, Pepa);
+            Fighter Pepa = new Fighter("Pepik", 110, 15, 15);
+            //Arena arena = new Arena(Jarda, Pepa);
 
+            fighters.Add(Jarda);
+            fighters.Add(Pepa);
+            fighters.Add(new Fighter("Ferda", 100, 20, 10));
+            fighters.Add(new Fighter("Venda", 100, 20, 10));
+            fighters.Add(new Fighter("Lužon", 100, 20, 10));
+            fighters.Add(new Fighter("Tomáš", 100, 20, 10));
+            fighters.Add(new Fighter("David", 100, 20, 10));
+            fighters.Add(new Fighter("Bohuslav", 100, 20, 10));
 
-
-            arena.Zapas();
+            //arena.Zapas();
             
 
 
---
+++ Fighting_game/Fighting_game/Turnament.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fighting_game
+{
+    internal class Turnament
+    {
+
+        public List<Fighter> fighters { get; private set; }
+
+
+        public Turnament(List<Fighter> fighters)
+        {
+            this.fighters = fighters;
+        }
+
+
+        public List<Arena> CreateTrees(List<Fighter> fighters)
+        {
+            List<Arena> Fight = new List<Arena>();
+
+            for(int i = 0; i < fighters.Count; i += 2)
+            {
+                Fight.Add(new Arena(fighters[i], fighters[i+1]));
+            }
+            return Fight;
+
+        }
+
+
+
+
+    }
+}
