2a031b38062d969bb2d0c64177a20aa5060a2d08
Ironknee <marekdarsa@gmail.com>
2022-11-28 14:41:09 +0100

    done

---
+++ Word_writing/Word_writing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_writing
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string slovo = "";
+            List<string> list = new List<string>();
+
+            while (slovo != "konec") 
+            {
+                Console.Write("Zadej Slovo: ");
+                slovo = Console.ReadLine();
+                if(slovo != "konec")
+                    list.Add(slovo);
+
+            }
+
+            Console.Write("Zadal jsi tato slova:");
+            foreach(string s in list)
+                Console.Write(s+", ");
+            Console.ReadKey();
+        }
+    }
+}
